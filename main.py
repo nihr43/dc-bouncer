0e71dc3be89dbfaf95c42db3b811de94f01de668
nathan hensel <nathanihensel@gmail.com>
2022-10-03 10:25:55 -0500

    flake8

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import json
 import time
+
 
 def k8s_ok(client, logging) -> bool:
     '''
@@ -14,7 +14,7 @@ def k8s_ok(client, logging) -> bool:
 
     for node in node_list.items:
         for i in node.status.conditions:
-            if i.type == 'Ready':        # these dont appear to be more easily addresable
+            if i.type == 'Ready':        # these dont appear to be more easily addresable # noqa
                 node_status = i.status
                 if node_status != 'True':
                     not_ready.append(node.metadata.name)
@@ -32,7 +32,7 @@ def ceph_ok(client, logging) -> bool:
     '''
     api = client.CustomObjectsApi()
 
-    # CephCluster crd defined at https://github.com/rook/rook/blob/master/deploy/examples/crds.yaml line 841
+    # CephCluster crd defined at https://github.com/rook/rook/blob/master/deploy/examples/crds.yaml line 841 # noqa
     resource = api.get_namespaced_custom_object(
         group="ceph.rook.io",
         version="v1",
@@ -59,18 +59,18 @@ def upgrade_node(ansible_runner, host, os):
 
     ansible_runner.run(
         private_data_dir='./',
-        inventory = host,
-        playbook = 'apt_upgrade.yml'
+        inventory=host,
+        playbook='apt_upgrade.yml'
     )
 
 
-def wait_until(fn):
+def wait_until(fn, logging):
     '''
     given a function, attempt a number of retries
     '''
     count = 100
     for i in range(count):
-        if fn() == True:
+        if fn():
             break
         if i == count:
             logging.info('timed out waiting for ' + fn().__name__)
@@ -80,29 +80,27 @@ def wait_until(fn):
 
 if __name__ == '__main__':
     def privileged_main():
-        from kubernetes import client, config, watch
+        from kubernetes import client, config
         import ansible_runner
 
         import logging
         import os
         from functools import partial
 
-        hosts = ['10.0.200.1','10.0.200.3','10.0.254.253']
+        hosts = ['10.0.200.1', '10.0.200.3', '10.0.254.253']
 
         logging.basicConfig(level=logging.INFO)
         config.load_kube_config()
 
-        notready = []
-
         k8s_ok_partial = partial(k8s_ok, client, logging)
         ceph_ok_partial = partial(ceph_ok, client, logging)
 
-        wait_until(partial_k8s_ok)
-        wait_until(partial_ceph_ok)
+        wait_until(k8s_ok_partial, logging)
+        wait_until(ceph_ok_partial, logging)
 
         for n in hosts:
             upgrade_node(ansible_runner, n, os)
-            time.sleep(30) # health doesnt necessarily take immediate effect
+            time.sleep(30)  # health doesnt necessarily take immediate effect
             wait_until(k8s_ok_partial)
             wait_until(ceph_ok_partial)
 
