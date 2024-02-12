103912fa0c88911da6f2a55fe8383b4f7bdaf5cc
nathan hensel <nathanihensel@gmail.com>
2024-02-12 00:07:34 -0600

    revert 131026

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def wait_until(fn: partial[bool], retry: int, success: int) -> None:
     if retry == 0:
         raise TimeoutError("timed out waiting")
 
-    time.sleep(2)
+    time.sleep(5)
 
     if fn():
         success -= 1
